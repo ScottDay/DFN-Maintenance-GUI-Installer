a9325e3a2565c328059a1df84e6bbaa47aeaccaa
Scott Day <17160182@student.curtin.edu.au>
2018-08-31 18:15:40 +1000

    feat(travis): basic travis setup with semantic release

---
+++ .travis/release.sh
@@ -0,0 +1,7 @@
+# Fetch repo tags from git.
+git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch --tags
+
+# Publish the new version tags to git.
+npm install
+npx semantic-release
