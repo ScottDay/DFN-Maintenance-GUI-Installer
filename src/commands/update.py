52fec5f08268c70fe7ace43a23961061f8e5543c
Scott Day <17160182@student.curtin.edu.au>
2018-09-05 16:45:52 +1000

    feat(update): dry update check

--- src/commands/update.py
+++ src/commands/update.py
@@ -1,43 +1,50 @@
+import re
+
 from src.util.logger import logger, section, debug
 from src.util.wrappers import wrapper
 from src.util.json import load_json
-from src.config import get_config
+
+
+__all__ = ['update']
 
 
 # TODO: Default to online, optionally pass a tar file or directory to check.
 # TODO: Shutdown the project if an update is available.
-# TODO: Option to just check for a new version. Don't download.
 @wrapper
 def update(args):
 	"""
 	Update the project to the latest version.
 	"""
-	conf = get_config()
+	update = check_version(args.conf)
 
-	# Download and check version information.
-	check_version(conf)
+	if not args.dry:
+		download_update(args.conf)
 
 
 @section('Checking version tags')
 def check_version(conf):
-	remote_version = load_json(conf.releaseUrl, keys = 'tag_name')
 	local_version = load_json(conf.installerPath, conf.envFile, keys = 'version.release')
+	remote_version = load_json(conf.releaseUrl, keys = 'tag_name')
 
-	return parse_tags(remote_version, local_version)
+	return parse_tags(local_version, remote_version)
 
 
 @debug('Parsing version tags')
-def parse_tags(remote, local):
-	import re
-
+def parse_tags(local, remote):
 	result = False
 
-	remote = re.sub('[^0-9]', '', remote)
 	local = re.sub('[^0-9]', '', local)
+	remote = re.sub('[^0-9]', '', remote)
 
-	logger.debug('Remote / local versions v{0}:v{1}'.format(remote, local))
+	logger.info('local:  v{0}'.format(local))
+	logger.info('remote: v{0}'.format(remote))
 
 	if remote > local:
 		result = True
 
 	return result
+
+
+@section('Downloading update')
+def download_update(conf):
+	logger.debug('test...')
--- src/util/wrappers.py
+++ src/util/wrappers.py
@@ -1,13 +1,17 @@
+from src.config import get_config, write_config
+from argh import arg, expects_obj
+from functools import wraps
 from .logger import logging, logger
 
 
-def wrapper(function):
-	from argh import arg, expects_obj
-	from functools import wraps
+__all__ = ['wrapper']
 
+
+def wrapper(function):
 	@arg('-s', '--silent', default = False, help = 'Suppress all output.')
 	@arg('-d', '--debug', default = False, help = 'Log all debug output.')
 	@arg('--dev', default = False, help = 'Use the development config "config/dev.json"')
+	@arg('--dry', default = False, help = 'Dry run, runs the command without committing any changes.')
 	@expects_obj
 	@wraps(function)
 	def wrapper(*args, **kwds):
@@ -21,20 +25,15 @@ def wrapper(function):
 
 			if args[0].dev:
 				logger.debug('Using development config...')
-				from src.config import get_config, write_config
 
 				conf = get_config('config/config.json', 'config/dev.json')
 				write_config(conf)
 
+			args[0].conf = get_config()
+
 			return function(*args, **kwds)
 		except Exception as error:
-			import sys, os
-
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			filename = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-			line_number = exc_tb.tb_lineno
-
-			logger.critical("{0} [{1}:{2}]: {3}".format(type(error).__name__, filename, line_number, str(error)))
+			logger.critical("{0}: {1}".format(type(error).__name__, str(error)))
 
 			if args[0].dev:
 				raise error
