1c5e7adec60d9255de55b8b9128cc2b83190f924
Scott Day <17160182@student.curtin.edu.au>
2018-09-05 12:15:58 +1000

    feat(logging): log exceptions and, in dev mode, log stack trace

--- src/commands/shared.py
+++ src/commands/shared.py
@@ -5,34 +5,41 @@ log = logging.getLogger()
 
 
 def wrapper(function):
-	from argh import arg, expects_obj, wrap_errors
+	from argh import arg, expects_obj
 	from functools import wraps
 
 	@arg('-s', '--silent', default = False, help = 'Suppress all output.')
 	@arg('-d', '--debug', default = False, help = 'Log all debug output.')
-	@arg('--dev', default = False, help = 'Use the development config "config/dev.json".')
-	@wrap_errors([Exception])
+	@arg('--dev', default = False, help = 'Use the development config "config/dev.json"')
 	@expects_obj
 	@wraps(function)
 	def wrapper(*args, **kwds):
-		import logging
+		try:
+			if args[0].silent:
+				logging.disabled = True
 
-		log = logging.getLogger()
+			if args[0].debug:
+				log.debug('Debug logging enabled...')
+				log.setLevel(logging.DEBUG)
 
-		if args[0].silent:
-			logging.disabled = True
+			if args[0].dev:
+				log.debug('Using development config...')
+				from src.config import get_config, write_config
 
-		if args[0].debug:
-			log.debug('Debug logging enabled...')
-			log.setLevel(logging.DEBUG)
+				conf = get_config('config/config.json', 'config/dev.json')
+				write_config(conf)
 
-		if args[0].dev:
-			log.debug('Using development config...')
-			from src.config import get_config, write_config
+			return function(*args, **kwds)
+		except Exception as error:
+			import sys, os
 
-			conf = get_config('config/config.json', 'config/dev.json')
-			write_config(conf)
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			filename = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			line_number = exc_tb.tb_lineno
 
-		return function(*args, **kwds)
+			log.critical("{0} [{1}:{2}]: {3}".format(type(error).__name__, filename, line_number, str(error)))
+
+			if args[0].dev:
+				raise error
 
 	return wrapper
