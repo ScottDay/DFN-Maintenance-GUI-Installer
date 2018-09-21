4a6a3e6331cb462370d700f5e8dc87c275a2405a
Scott Day <17160182@student.curtin.edu.au>
2018-09-21 13:51:06 +1000

    chore(requirements): bump all versions to latest

--- src/commands/update/__init__.py
+++ src/commands/update/__init__.py
@@ -10,6 +10,12 @@ __all__ = ['update']
 
 # TODO: Default to online, optionally pass a tar file or directory to check.
 # TODO: Shutdown the project if an update is available.
+# TODO: Compare requirements.txt files and see if dependencies need to be installed (any there are any differences).
+# TODO: Self update command (seperate to gui update)
+# TODO: Use this update command to update everything, specific commands for each project (update-installer, update-gui, etc.)
+# TODO: Command to update to the dev version. Tie in with the specified projects dev version (installer dev version update, or gui dev version update).
+# TODO: Command to use docker version.
+# TODO: Place a installer config file in each project, use it to specify how to install and what commands to run.
 @wrapper
 def update(args):
 	"""
